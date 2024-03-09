b3d48e9872fa5d9366ff1fff88fe72b3d6227867
lakhbir <lakhbir.shah@yahoo.co.uk>
2024-03-09 22:39:11 +0000

    added connection to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import "dotenv/config";
+
+require("dotenv").config();
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to DB!"));
 
 const app = express();
 
@@ -12,5 +18,5 @@ app.get("/test", async (req: Request, res: Response) => {
 });
 
 app.listen(7000, () => {
-  console.log("Server started on port");
+  console.log("Server started on port 7000");
 });
