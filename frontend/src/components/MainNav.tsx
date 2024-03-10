5e79118f4090944b0d9134bba46b500167540548
lakhbir <lakhbir.shah@yahoo.co.uk>
2024-03-10 11:32:00 +0000

    main and mobile nav set-up

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./ui/button";
+
+export default function MainNav() {
+  return (
+    <Button
+      className="font-bold hover:text-orange-500 hover:bg-white"
+      variant="ghost"
+    >
+      Log In
+    </Button>
+  );
+}
---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Button } from "./ui/button";
+import { Separator } from "@radix-ui/react-separator";
+
+export default function MobileNav() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle>
+          <span>Welcome to Food.com</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+}
